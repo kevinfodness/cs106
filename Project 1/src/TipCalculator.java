37d5286f2526f7f924f143b7822cf2b3e62091f7
Kevin Fodness <kfodness@fingerpaintmarketing.com>
2014-09-10 17:45:14 -0400

    Scaffolded the basis of project 1 - tip calculator.

---
+++ Project 1/src/TipCalculator.java
@@ -0,0 +1,17 @@
+/**
+ * A program to calculate tips based on various scenarios.
+ * 
+ * @author Kevin Fodness <kevin@kevinfodness.com>
+ * @version 1.0
+ */
+public class TipCalculator {
+
+	/**
+	 * Constructor function. Initializes the program.
+	 * 
+	 * @param args Arguments passed to the constructor.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Scaffolded tip calculator.");
+	}
+}
