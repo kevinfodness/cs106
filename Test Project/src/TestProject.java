2bcb8d9f5ca1eb64f7f78bf50af320a72d544e16
Kevin Fodness <kfodness@fingerpaintmarketing.com>
2014-09-14 20:30:58 -0400

    Created test project for beta-testing code prior to adding to lecture slides to prevent code typos.

---
+++ Test Project/src/TestProject.java
@@ -0,0 +1,15 @@
+/**
+ * A project in which to test code before adding it to lecture slides.
+ * 
+ * @author Kevin Fodness <kevin@kevinfodness.com>
+ */
+public class TestProject {
+
+	/**
+	 * The main method, responsible for doing all the things.
+	 * 
+	 * @param args Arguments passed to the program.
+	 */
+	public static void main(String[] args) {
+	}
+}
