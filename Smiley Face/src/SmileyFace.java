d82a10950783d35c0ee1fc343f77ee449d2f4d4b
Kevin Fodness <kfodness@fingerpaintmarketing.com>
2014-09-19 10:23:57 -0400

    Created smiley face program.

---
+++ Smiley Face/src/SmileyFace.java
@@ -0,0 +1,17 @@
+/**
+ * Contains a program to create a smiley face.
+ * 
+ * @author Kevin Fodness <kevin@kevinfodness.com>
+ * @version 1.0
+ */
+public class SmileyFace {
+
+	/**
+	 * Main method - does all the things.
+	 * 
+	 * @param args Arguments passed to the program.
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
