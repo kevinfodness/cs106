940eee99365ae4af35d74ab0620c24b996beed21
Kevin Fodness <kevin@kevinfodness.com>
2014-11-13 22:12:46 -0500

    Added example with HashMap.

--- Lecture Examples/src/LectureExamples.java
+++ Lecture Examples/src/LectureExamples.java
@@ -1,5 +1,25 @@
-public class LectureExamples {
-    public static void main(String[] args) {
+import java.util.*;
 
+/**
+ * Contains a class to represent a program to test code for lecture slides.
+ *
+ * @author Kevin Fodness <kevin@kevinfodness.com>
+ */
+public class LectureExamples {
+
+    /**
+     * Main method. Directs control of the program.
+     *
+     * @param args Command-line arguments.
+     */
+    public static void main(String[] args) {
+        HashMap<String, String> stocks = new HashMap<String, String>();
+        stocks.put("AAPL", "Apple, Inc.");
+        stocks.put("GE", "General Electric");
+        stocks.put("GOOG", "Google");
+        Collection<String> values = stocks.values();
+        for (String s : values) {
+            System.out.println(s);
+        }
     }
 }
