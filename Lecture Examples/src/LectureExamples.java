166c8a44c007001491c1f78073c2591efe0775ee
Kevin Fodness <kevin@kevinfodness.com>
2014-11-18 20:05:25 -0500

    Added examples for sum of squares and Fibonacci sequence.

--- Lecture Examples/src/LectureExamples.java
+++ Lecture Examples/src/LectureExamples.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 /**
  * Contains a class to represent a program to test code for lecture slides.
  *
@@ -13,13 +11,41 @@ public class LectureExamples {
      * @param args Command-line arguments.
      */
     public static void main(String[] args) {
-        HashMap<String, String> stocks = new HashMap<String, String>();
-        stocks.put("AAPL", "Apple, Inc.");
-        stocks.put("GE", "General Electric");
-        stocks.put("GOOG", "Google");
-        Collection<String> values = stocks.values();
-        for (String s : values) {
-            System.out.println(s);
+        System.out.println(sumOfSquares(7));
+        fibonacci(0, 1, 1000);
+    }
+
+    /**
+     * A recursive method to compute the sum of squares.
+     *
+     * @param max The maximum number of squares to return.
+     * @return The sum of the current square plus the next one, if applicable.
+     */
+    public static int sumOfSquares(int max) {
+        /* As long as there are still numbers left, add the next square. */
+        if (max > 1) {
+            return max * max + sumOfSquares(max - 1);
+        }
+
+        return 1;
+    }
+
+    /**
+     * Recursive method to compute Fibonacci numbers.
+     *
+     * @param current The current number in sequence.
+     * @param next    The next number in sequence.
+     * @param max     The maximum number to compute.
+     */
+    public static void fibonacci(int current, int next, int max) {
+        if (next < max) {
+            int total = current + next;
+            current = next;
+            next = total;
+            System.out.print(current + " ");
+            fibonacci(current, next, max);
+        } else {
+            System.out.println();
         }
     }
 }
